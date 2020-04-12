d43da06bbc1757030932ff9024c001bffb7c5618
Douglas Neves <d88.neves@gmail.com>
2020-04-12 18:16:12 -0300

    fix delete

--- src/app.js
+++ src/app.js
@@ -42,11 +42,12 @@ app.put("/repositories/:id", (request, response) => {
 
 app.delete("/repositories/:id", (request, response) => {
   const { id } = request.params;
-  const repository = repositories.find(repository => repository.id === id);
-  if (!repository) {
+  const repositoryIndex = repositories.findIndex(repository => repository.id === id);
+  if (repositoryIndex >= 0) {
+    repositories.splice(repositoryIndex, 1);
+  } else {
     response.status(400).send('Unidentified id');
   }
-  repositories.splice(repository);
   return response.status(204).send();
 });
 
